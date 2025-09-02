5e27269f6084f7125a77d9ec58d414c75c98ce52
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2025-09-02 20:00:37 +0300

    Add files via upload

---
+++ ShopTARgv24.Data/ShopTARgv24Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARgv24.Core.Domain;
+
+namespace ShopTARgv24.Data
+{
+    internal class ShopTARgv24Context : DbContext
+    {
+
+        public ShopTARgv24Context(DbContextOptions<ShopTARgv24Context> options)
+        :base (options) { }
+        public DbSet<Spaceship> Spaceships { get; set; }
+    }
+}
